ce658a72ab4ce19fdcd7b9dd217ac7bd332c0f02
Tim Swast <swast@google.com>
2020-07-20 12:17:04 -0500

    Avoid thirdparty directory

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,9 +6,13 @@ if errorlevel 1 exit 1
 :: Configure and install based on other `bld.bat`s and
 ::https://github.com/google/crc32c/blob/master/.appveyor.yml
 cmake -G "Ninja" ^
+         -DBUILD_TESTING=OFF ^
+         -DBUILD_SHARED_LIBS=ON ^
          -DCMAKE_BUILD_TYPE=Release ^
          -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
          -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+         -DCRC32C_BUILD_TESTS=OFF ^
+         -DCRC32C_BUILD_BENCHMARKS=OFF ^
          -DCRC32C_USE_GLOG=0 ^
          ..
 if errorlevel 1 exit 1
